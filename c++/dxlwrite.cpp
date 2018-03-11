0d025b06948f1cf72bec6cc0ba82963f739435f4
Piyush Jaipuriyar <piyushjaipuriyar@gmail.com>
2018-03-11 16:20:50 +0530

    Some changes

--- c++/dxlwrite.cpp
+++ c++/dxlwrite.cpp
@@ -7,10 +7,15 @@ int main(int argc, char *argv[])
 {
   string file = "/dev/ttyUSB";
   vector<string> ports = get_available_ports(file);
+  if(ports.size() == 0)
+  {  
+    std::cout << "No device connected\n";
+    return 0;
+  }
   Dxl dxl(ports[0]);
   vector<int> ids = dxl.scan(25);
   int pos = 100;
-  dxl.write(9, pos, 100);
-  dxl.write(17, -pos, 100);
+  dxl.write(9, 100.0, 300);
+  dxl.write(17, -100.0, 300);
   return 0;
 }
