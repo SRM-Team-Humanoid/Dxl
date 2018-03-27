a3cb6915eab9e2ae212179f25fd3b368d558fda0
Piyush Jaipuriyar <piyushjaipuriyar@gmail.com>
2018-03-27 21:04:27 +0530

    Added stuff

--- python/try.py
+++ python/try.py
@@ -1,10 +1,19 @@
 import dxl as Dxl
-
+import math
 ports = Dxl.get_available_ports()
 print "connecting to port: %s" % (ports[0])
 
 dxl = Dxl.Dxl(ports[0])
 
 ids = dxl.scan(25)
-
-dxl.set_goal_position()
+print ids
+dxl.enable_torque(ids)
+beg = end = 100
+for pos in range(-90, 91, 3):
+    speed = -int(1000 * (math.sin(math.pi * (pos - 90) / 180)))
+    dxl.set_moving_speed({1: speed})
+    dxl.set_goal_position({1:pos})
+for pos in range(90, -91, -3):
+    speed = -int(1000 * (math.sin(math.pi * (pos - 90) / 180)))
+    dxl.set_moving_speed({1: speed})
+    dxl.set_goal_position({1:pos})
